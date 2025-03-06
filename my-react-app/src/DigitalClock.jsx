28c652f3961fe843221a366b430db0940e7bc08e
Edward Ofosu Mensah <edward.mensah@ashesi.edu.gh>
2025-03-06 03:03:37 +0000

    Built a simple digital clock using the concepts learnt from useState and useContext

--- my-react-app/src/DigitalClock.jsx
+++ my-react-app/src/DigitalClock.jsx
@@ -1,4 +1,27 @@
-import { func } from 'prop-types';
+/* body{
+    background-image: url(./assets/forestbg.jpeg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.clock-container{
+    backdrop-filter: blur(2px);
+    padding:10px 0;
+}
+
+.clock{
+    color: white;
+    font-size: 50px;
+    font-family: monospace;
+    font-weight: bold;
+} */
 import React, { useState, useEffect } from 'react';
 function DigitalClock() {
   const [time, setTime] = useState(new Date());
